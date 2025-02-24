f17b3b1b4f7be5f59809bfc490e744010336be93
ToiMaiTaiMoi_Tum <iloveusomuchx2@gmail.com>
2025-02-24 17:30:40 +0700

    "lab22 finised"

--- lab22.cpp
+++ lab22.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
 #include<cmath>
+#include<string>
+#include<stdio.h>
 using namespace std;
 
 class ComplexNumber{				
@@ -29,6 +31,46 @@ ComplexNumber ComplexNumber::operator-(const ComplexNumber &c){
 }
 
 //Write your code here
+ostream & operator<<(ostream &os, const ComplexNumber &c){
+	if(c.imag < 0 && c.real != 0) return os << c.real << "-" << (-1) * c.imag << "i";
+	else if(c.imag < 0 && c.real == 0) return os << "-"<< -c.imag << "i";
+	else if(c.imag == 0) return os << c.real ;
+	else if(c.real == 0) return os << c.imag << "i" ;
+	else return os <<c.real << "+" << c.imag << "i";
+}
+ComplexNumber operator+(double s, const ComplexNumber &c){
+	return ComplexNumber(s+c.real,c.imag);
+}
+ComplexNumber operator-(double s, const ComplexNumber &c){
+	return ComplexNumber(s-c.real,-c.imag);
+}
+ComplexNumber ComplexNumber::operator/(const ComplexNumber &c){
+	return ComplexNumber((c.real * real + c.imag*imag) / (pow(c.real,2) + pow(c.imag,2)) , (imag * c.real - real*c.imag) / (pow(c.real,2) + pow(c.imag,2)));
+}
+ComplexNumber ComplexNumber::operator*(const ComplexNumber &c){
+	return ComplexNumber((c.real * real - c.imag * imag ), (c.real*imag + c.imag*real));
+}
+ComplexNumber operator/(double s, const ComplexNumber &c){
+	return ComplexNumber((s*c.real) / (pow(c.imag,2) + pow(c.real,2)),s * -c.imag / (pow(c.imag,2) + pow(c.real,2)));
+}
+ComplexNumber operator*(double s, const ComplexNumber &c){
+	return ComplexNumber(s * c.real,s *c.imag);
+}
+double ComplexNumber::abs(){
+	return sqrt(pow(real,2) + pow(imag,2));
+}
+double ComplexNumber::angle(){
+	return atan2(imag,real) * 180 / 3.14159265;
+}
+bool ComplexNumber::operator==(const ComplexNumber &c){
+	if(real == c.real && imag == c.imag) return true;
+	else return false;
+}
+bool operator==(double s, const ComplexNumber &c){
+	if(s == c.real && s == c.imag) return true;
+	else return false;
+}
+
 
 int main(){
 	ComplexNumber a(1.5,2),b(3.2,-2.5),c(-1,1.2);	
@@ -42,7 +84,6 @@ int main(){
 	cout << b+ComplexNumber(0,2.5) << "\n";
 	cout << c-c << "\n";
 	cout << "-----------------------------------\n";
-	
 	ComplexNumber d = (a+b)/c;
 	ComplexNumber e = b/(a-c);
 	cout << d << "\n";
@@ -51,7 +92,6 @@ int main(){
 	cout << 0.5*c << "\n";
 	cout << 1/c << "\n";
 	cout << "-----------------------------------\n";
-	
 	cout << ComplexNumber(1,1).abs() << "\n";
 	cout << ComplexNumber(-1,1).abs() << "\n";
 	cout << ComplexNumber(1.5,2.4).abs() << "\n";
